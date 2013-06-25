e292d30c558b26c6033029ff00256c4a03749391
Tom Lambert <tom.lambert@dataflow.be>
2013-06-25 13:27:46 +0200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
    @user = User.find(params[:id]) 
+   @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -47,13 +48,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def sign_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: 'Please sign in.'
-      end
-    end
 
     def signed_in_user
       if signed_in?
